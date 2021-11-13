5834396cae93056cdd2b633efd9477e1075dd0e8
Igor Pereira <igorpereira.dev@gmail.com>
2021-11-13 17:56:03 -0300

    Bug da lampada que não queria acender resolvido

--- lampada.js
+++ lampada.js
@@ -1,6 +1,8 @@
-const turnOn = document.getElementById('turnOn');
-const turnOff = docment.getElementById('turnOff');
-const lamp = document.getElementById('lamp');
+const turnOn = document.getElementById ( 'turnOn' );
+const turnOff = document.getElementById ( 'turnOff' );
+const lamp = document.getElementById ( 'lamp' );
+
+
 
 function lampOn(){
     lamp.src = 'img/ligada.jpg';
